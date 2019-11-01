98346f3d97e4dabcd968910af5f52b14c8205604
Misha Hnatyshyn <31630903+MishaHnatyshyn@users.noreply.github.com>
2019-11-01 17:28:24 +0200

    fix CORS issue (#7)

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import {ValidationPipe} from "@nestjs/common";
+import { ValidationPipe } from '@nestjs/common';
 
 const port = process.env.PORT || 3002;
 
@@ -13,6 +13,7 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(port, '0.0.0.0');
 }
