6cd506155fc00e6a57de658ac50dd75efb7e70bd
VladaZhmurkevych <vladagmyrka1@gmail.com>
2019-12-10 02:01:45 +0200

    Add tests

--- src/modules/models/models.service.ts
+++ src/modules/models/models.service.ts
@@ -27,33 +27,37 @@ export class ModelsService {
     return { count };
   }
 
+  async createModelInSingleTransaction(data: CreateModelDto, admin: number, manager) {
+    const { name, connectionId, items, relations } = data;
+    const model = await this.modelsRepositoryService.createModel(name, admin, connectionId, manager);
+    const { id: modelId } = model;
+    const modelItems = await Promise.all(items.map(item => this.createModelItem(item, modelId, manager)));
+    const modelItemsMap = ModelsService.createModelItemsMapForRelations(modelItems);
+    const relationsWithModelsId = ModelsService.patchRelationsDataWithModelsId(modelItemsMap, relations);
+    await Promise.all(
+      relationsWithModelsId.map(relation =>
+        this.modelItemsRelationRepositoryService.createRelation({ ...relation, connectionManager: manager }),
+      ),
+    );
+    return {
+      id: modelId,
+      name: model.name,
+      tables: modelItems.length,
+      fields: items.reduce((acc, curr) => acc + curr.rows.length, 0),
+      users: 0,
+      uses: 0,
+      active: true,
+      created: model.created_at,
+    };
+  }
+
   async createModel(data: CreateModelDto, admin: number) {
     return await getManager().transaction(async manager => {
-      const { name, connectionId, items, relations } = data;
-      const model = await this.modelsRepositoryService.createModel(name, admin, connectionId, manager);
-      const { id: modelId } = model;
-      const modelItems = await Promise.all(items.map(item => this.createModelItem(item, modelId, manager)));
-      const modelItemsMap = ModelsService.createModelItemsMapForRelations(modelItems);
-      const relationsWithModelsId = ModelsService.patchRelationsDataWithModelsId(modelItemsMap, relations);
-      await Promise.all(
-        relationsWithModelsId.map(relation =>
-          this.modelItemsRelationRepositoryService.createRelation({ ...relation, connectionManager: manager }),
-        ),
-      );
-      return {
-        id: modelId,
-        name: model.name,
-        tables: modelItems.length,
-        fields: items.reduce((acc, curr) => acc + curr.rows.length, 0),
-        users: 0,
-        uses: 0,
-        active: true,
-        created: model.created_at,
-      };
+      return this.createModelInSingleTransaction(data, admin, manager);
     });
   }
 
-  private static patchRelationsDataWithModelsId(modelItemsMap: Map<string, number>, relations: RelationItem[]) {
+  static patchRelationsDataWithModelsId(modelItemsMap: Map<string, number>, relations: RelationItem[]) {
     return relations.map((relation: RelationItem) => ({
       firstModelItemId: modelItemsMap.get(relation.firstTableName),
       secondModelItemId: modelItemsMap.get(relation.secondTableName),
@@ -62,14 +66,14 @@ export class ModelsService {
     }));
   }
 
-  private static createModelItemsMapForRelations(modelItems): Map<string, number> {
+  static createModelItemsMapForRelations(modelItems): Map<string, number> {
     return modelItems.reduce((map, curr) => {
       map.set(curr.table_name, curr.id);
       return map;
     }, new Map());
   }
 
-  private async createModelItem(modelItemData: ModelItem, modelId: number, connectionManager: EntityManager) {
+  async createModelItem(modelItemData: ModelItem, modelId: number, connectionManager: EntityManager) {
     const { tableName, name, rows } = modelItemData;
     const modelItem = await this.modelItemsRepositoryService.createModelItem(
       name,
